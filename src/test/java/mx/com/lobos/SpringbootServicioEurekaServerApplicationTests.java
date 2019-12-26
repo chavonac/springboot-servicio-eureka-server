c448227dedb1e46d936256fda1cfbd84dfce82a7
chavonac <chavonbassguitar@hotmail.com>
2019-12-25 18:07:20 -0600

    Curso microservicios Seccion 3 capitulo 23

---
+++ src/test/java/mx/com/lobos/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.lobos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
